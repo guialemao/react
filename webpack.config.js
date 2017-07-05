b207eb8158445d8603f79c482a3084b6ff77ad22
Guilherme Milfont Moreno <guilherme.milfont@gmail.com>
2017-07-05 14:36:44 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, './src/assets/js'),
+  entry: {
+    app: './main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'js/[name].bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, './src'), 
+      'node_modules'
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './src'),  // New
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015'] },
+        }],
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader', 
+            'sass-loader',
+            'import-glob-loader'
+          ],
+        })
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        use: {
+          loader: 'url-loader'
+        }
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      title: 'Boiler',
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html'
+    }),
+    new ExtractTextPlugin({
+      filename: 'css/style.css'
+    })  
+  ]
+};
