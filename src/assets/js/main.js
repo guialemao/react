056505ed01cd468001b91844ba97aa37f206d505
Guilherme Milfont Moreno <guilhermemilfont@MacBook-Pro-de-Guilherme.local>
2017-07-05 23:11:25 -0300

    prepared template to react

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,14 @@
 import '../sass/main.scss';
+
+import React from 'react';
+import { render } from 'react-dom';
+
+console.log(React);
+
+const p1 = (
+  <div>
+    <h1>Hello world</h1>
+  </div>
+);
+
+console.log(p1);
