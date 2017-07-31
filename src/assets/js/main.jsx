96d5feccac511466e2a2fa3d49e940d363ff7896
Guilherme Milfont Moreno <guilherme.milfont@gmail.com>
2017-07-31 15:44:00 -0300

    Ajustado estrutura de componente do ToDo List

--- src/assets/js/main.jsx
+++ src/assets/js/main.jsx
@@ -21,40 +21,6 @@ let saveData = [
 	}
 ];
 
-// function tic () {
-//   const comp1 = (
-//     <div>
-//       <h1>My first component</h1>
-//       <h2>It's {new Date().toLocaleTimeString()}</h2>
-//     </div>
-//   );
-//   render(
-//     comp1,
-//     document.querySelector("#clock"),
-//   );
-// }
-
-// setInterval(tic, 1000);
-
-// function Hello(props) {
-//   return <h1>Hello, {props.name}</h1>;
-// }
-
-// function App() {
-//   return (
-//     <div>
-//       <Hello name="Guilherme" />
-//       <Hello name="Juliana" />
-//       <Hello name="Joaquim" />
-//     </div>  
-//   )
-// }
-
-// render(
-//   <App />,
-//   document.querySelector("#hi"),
-// );
-
 
 function Tit(props) {
   return (
@@ -64,27 +30,17 @@ function Tit(props) {
 
 
 function BuildTitle() {
-  const tits = saveData.map(function(a,i){
+  const tits = saveData.map((a,i) => {
     return a.titleTask;
   });
-  console.log(tits);
   return (
     <div>
       {tits.map((name) => <Tit key={name} name={name} />)}
     </div>  
   ) 
-
 }
 
 render(
   <BuildTitle />,
   document.querySelector("#todo-results"),
 )
-
-// function t() {
-
-//   const tt = saveData.filter(function(h){
-//     return h.titleTask;
-//   })
-
-// }
